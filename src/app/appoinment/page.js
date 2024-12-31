4b917f067913a0cefa117f9a752a73c161f5d332
musa-qadri <musaqazim1@gmail.com>
2024-12-31 15:03:26 +0500

    uuu

--- src/app/appoinment/page.js
+++ src/app/appoinment/page.js
@@ -8,7 +8,6 @@ import { appointments } from '@/lib/data'
 export default function appoinments() {
   return (
     <div className='container mx-auto mt-9'>
-      
       <AppoinmentTable columns={columns} data={appointments} />
 
     </div>
